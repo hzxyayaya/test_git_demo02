caadfc08530bd29be4b878749f59359cd16c9ca8
huzhaoxuan <3445267935@qq.com>
2024-05-19 21:24:33 +0800

    hot-fix commit2

--- src/main/java/com/Javaproject/Main.java
+++ src/main/java/com/Javaproject/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.println("Hello and welcome!");
         System.out.println("Hello and welcome!");
         System.out.println("Hello and welcome!");
-        System.out.println("Hello and welcome!");
+        System.out.println("hot-fix test");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
