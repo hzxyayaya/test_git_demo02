9ef824a1ad20bb6d6df1dbe6d41d56d8a6da7efd
huzhaoxuan <3445267935@qq.com>
2024-05-19 22:25:24 +0800

    push commit

--- src/main/java/com/Javaproject/Main.java
+++ src/main/java/com/Javaproject/Main.java
@@ -12,6 +12,7 @@ public class Main {
         System.out.println("Hello and welcome!");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push commit");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
